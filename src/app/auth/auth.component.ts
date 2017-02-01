262a056a35b02ce39f29685b32d950d75015c495
Frankie Gavin <frankanthonygavin@gmail.com>
2017-02-01 15:24:12 -0800

    minor

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-auth',
-//   templateUrl: './auth.component.html',
-//   styleUrls: ['./auth.component.css']
-// })
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
 export class AuthComponent implements OnInit {
 
   constructor() { }
